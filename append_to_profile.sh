b616d99784a9cf264556923818064735051f99c2
illinoisjackson <max.nurzia@gmail.com>
2017-06-02 14:36:23 -0400

    Make options work again.

--- append_to_profile.sh
+++ append_to_profile.sh
@@ -1,15 +1,15 @@
 LS_COLORS=$(ls_colors_generator)
 
 run_ls() {
-	ls-i --color=auto -w $(tput cols)
+	ls-i --color=auto -w $(tput cols) "$@"
 }
 
 run_dir() {
-	dir-i --color=auto -w $(tput cols)
+	dir-i --color=auto -w $(tput cols) "$@"
 }
 
 run_vdir() {
-	vdir-i --color=auto -w $(tput cols)
+	vdir-i --color=auto -w $(tput cols) "$@"
 }
 alias ls="run_ls"
 alias dir="run_dir"
