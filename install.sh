6dadce8525843026e3e903b3bfd335db3721aa88
Joël COSTAMAGNA <joel.costamagna@protonmail.com>
2017-06-06 14:31:20 +0200

    modified to work with macOS

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-wget http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
-chmod 777 ls_colors_generator.py
+curl -o ls_colors_generator.py http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
+chmod 755 ls_colors_generator.py
 sudo mv ls_colors_generator.py /usr/bin/ls_colors_generator
-wget http://ftp.gnu.org/gnu/coreutils/coreutils-8.2.tar.xz
+curl -o coreutils-8.2.tar.xz http://ftp.gnu.org/gnu/coreutils/coreutils-8.2.tar.xz
 tar -xf coreutils-8.2.tar.xz
 rm coreutils-8.2.tar.xz
 cd coreutils-8.2/src
 rm -rf ls.c
-wget http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls.c
+curl -o ls.c http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls.c
 cd ..
-./configure
+./configure gl_cv_func_stpncpy=yes
 make
 cd src
 sudo mv ls /usr/bin/ls-i 
