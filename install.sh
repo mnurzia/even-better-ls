fd6aefd786214518344ea559ab7501fbacb49a37
Mateusz Ż <thedead4fun@live.com>
2017-06-26 00:19:45 +0200

    Change generator's permissions
    I changed 777 to 755 due to security reasons. Also removed weird
    whitespace at the end one line.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 wget http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
-chmod 777 ls_colors_generator.py
+chmod 755 ls_colors_generator.py
 sudo mv ls_colors_generator.py /usr/bin/ls_colors_generator
 wget http://ftp.gnu.org/gnu/coreutils/coreutils-8.2.tar.xz
 tar -xf coreutils-8.2.tar.xz
@@ -11,6 +11,6 @@ cd ..
 ./configure
 make
 cd src
-sudo mv ls /usr/bin/ls-i 
+sudo mv ls /usr/bin/ls-i
 sudo mv dir /usr/bin/dir-i
 sudo mv vdir /usr/bin/vdir-i
