24b613043a9d24871f271bac39533b44a3541c49
Oliver Leaver-Smith <oliver@leaversmith.com>
2017-06-28 16:58:49 +0100

    Missed the download method for the ls_colors_generator.py file

--- install.sh
+++ install.sh
@@ -3,18 +3,20 @@ coreutils_version="coreutils-8.2"
 case $OSTYPE in
     darwin*)
         macOS_config_params="gl_cv_func_stpncpy=yes"
+        python_download_method="curl -o ls_colors_generator.py"
         coreutils_download_method="curl -o ${coreutils_version}.tar.xz"
         patch_download_method="curl -o ls.c"
         install_location="/usr/local/bin"
         ;;
     *)
         install_location="/usr/bin"
+        python_download_method="wget"
         coreutils_download_method="wget"
         patch_download_method="wget"
         ;;
 esac
 
-wget http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
+$python_download_method http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
 chmod 755 ls_colors_generator.py
 sudo mv ls_colors_generator.py /usr/bin/ls_colors_generator
 $coreutils_download_method http://ftp.gnu.org/gnu/coreutils/${coreutils_version}.tar.xz
