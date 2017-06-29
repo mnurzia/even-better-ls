9a266e5e4212ec9b32699f3c89e7cf684689bac9
Oliver Leaver-Smith <oliver.leaver-smith@skybettingandgaming.com>
2017-06-29 16:14:54 +0100

    Make download method cleaner

--- install.sh
+++ install.sh
@@ -3,28 +3,24 @@ coreutils_version="coreutils-8.2"
 case $OSTYPE in
     darwin*)
         macOS_config_params="gl_cv_func_stpncpy=yes"
-        python_download_method="curl -o ls_colors_generator.py"
-        coreutils_download_method="curl -o ${coreutils_version}.tar.xz"
-        patch_download_method="curl -o ls.c"
+        download_method="curl -o"
         install_location="/usr/local/bin"
         ;;
     *)
+        download_method="wget -O"
         install_location="/usr/bin"
-        python_download_method="wget"
-        coreutils_download_method="wget"
-        patch_download_method="wget"
         ;;
 esac
 
-$python_download_method http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
+$download_method ls_colors_generator.py http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
 chmod 755 ls_colors_generator.py
 sudo mv ls_colors_generator.py ${install_location}/ls_colors_generator
-$coreutils_download_method http://ftp.gnu.org/gnu/coreutils/${coreutils_version}.tar.xz
+$download_method ${coreutils_version}.tar.xz http://ftp.gnu.org/gnu/coreutils/${coreutils_version}.tar.xz
 tar -xf ${coreutils_version}.tar.xz
 rm ${coreutils_version}.tar.xz
 cd ${coreutils_version}/src
 rm -rf ls.c
-$patch_download_method http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls.c
+$download_method ls.c http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls.c
 cd ..
 ./configure $macOS_config_params
 make
