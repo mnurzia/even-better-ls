1ab18a3676c126607fb336e584b16dfda70cad9c
Dorian Karter <dkarter@users.noreply.github.com>
2017-12-31 16:49:49 -0600

    Use HTTPS/SSL for downloading files
    This is recommended to avoid MITM attacks - critically important when running scripts from the web.

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-wget http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
+wget https://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls_colors_generator.py
 chmod 755 ls_colors_generator.py
 sudo mv ls_colors_generator.py /usr/bin/ls_colors_generator
-wget http://ftp.gnu.org/gnu/coreutils/coreutils-8.2.tar.xz
-tar -xf coreutils-8.2.tar.xz
-rm coreutils-8.2.tar.xz
-cd coreutils-8.2/src
+wget https://ftp.gnu.org/gnu/coreutils/coreutils-8.29.tar.xz
+tar -xf coreutils-8.29.tar.xz
+rm coreutils-8.29.tar.xz
+cd coreutils-8.29/src
 rm -rf ls.c
-wget http://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls.c
+wget https://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/ls.c
 cd ..
 ./configure
 make
